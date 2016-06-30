986d291485f634f333e466564e83c0d81fdbae51
Gabriel Adomnicai <gabesoft@gmail.com>
2016-06-30 08:55:13 -0700

    Emacs settings

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((haskell-process-args-cabal-repl . ("--ghc-option=-ferror-spans"
+                                             "--with-ghc=ghci-ng"
+                                             "--ghc-option=-package ghci-pretty" )))))

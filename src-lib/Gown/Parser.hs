63db333f94fc20787562d598d263f3013822b76c
Gabriel Adomnicai <gabesoft@gmail.com>
2016-06-30 18:41:09 -0700

    Parser fixes

--- src-lib/Gown/Parser.hs
+++ src-lib/Gown/Parser.hs
@@ -6,7 +6,7 @@ module Gown.Parser
 
 import Text.ParserCombinators.Parsec
 import Text.Parsec.Char (endOfLine)
-import Control.Applicative((*>),(<*),(<*>),(<$>))
+import Control.Applicative ((*>), (<*), (<*>), (<$>))
 
 data AclEntry =
   AclEntry {aclFile :: FilePath
@@ -20,14 +20,12 @@ data AclOwners =
 
 -- | Parses the owners section from the output of a git owners command
 acls :: CharParser () [AclEntry]
-acls = header *> many fileOwners <* rest
-  where header = string "OWNERS:" <* eol
-        rest = (many upper) <* char ':' <* many anyChar
+acls = (string "OWNERS:" <* eol) *> many1 fileOwners
 
 -- | Parses one entry of the owners section from the output of a git owners command
 -- | It returns the file path and the associated acl owners
 fileOwners :: CharParser () AclEntry
-fileOwners = AclEntry <$> (filePath <* eol) <*> (many1 $ try $ acl <* eol)
+fileOwners = AclEntry <$> (filePath <* eol) <*> (many1 $ try $ acl <* end)
 
 -- | Parses an acl type and the owners belonging to this type
 acl :: CharParser () AclOwners
@@ -57,6 +55,9 @@ semi = char ':'
 
 eol :: CharParser () Char
 eol = endOfLine
+
+end :: CharParser () ()
+end = (eol >> return ()) <|> eof
 
 nameExcluded
   :: String -> String -> CharParser () Char
